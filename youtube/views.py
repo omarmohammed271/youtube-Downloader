f45953ff881de55637a6cda28161d56fcb045614
moooooora <omarmohammed271@gmail.com>
2024-01-19 20:42:16 +0200

    Finished our website

--- youtube/views.py
+++ youtube/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
+from pytube import YouTube
 # Create your views here.
 def download(request):
-    
+    if request.method == 'POST':
+        url = request.POST['url']
+        # url = 'https://youtu.be/CUdQIT_Bj1s'
+        res = request.POST['video']
+        print(url)
+        print(res)
+        if url:
+            yt = YouTube(url)
+            if res == 'audio':
+                stream = yt.streams.get_audio_only()
+                stream.download('downloads/')
+            elif res == '1080p50':
+                stream = yt.streams.get_highest_resolution()
+                stream.download('downloads/')
+            else:
+                stream = yt.streams.get_by_resolution(resolution=res)    
+                stream.download('downloads/')
+
     return render(request,'home.html')
